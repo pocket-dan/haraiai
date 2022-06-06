9e8529dd3f241585a10b7d688cbf3aa5b6e943ad
raahii <piyo56.net@gmail.com>
2022-06-06 23:46:58 +0900

    Setup GA.

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from "vue-gtag"
 
 const app = createApp(App)
 
 app.use(router)
+
+if (import.meta.env.PROD) {
+  app.use(VueGtag, {
+    config: {id: "G-FMS9YJCVM8"}
+  }, router);
+}
 
 app.mount('#app')
