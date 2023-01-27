9548b49c939265e728a291013f706bf29ee2af6d
raahii <piyo56.net@gmail.com>
2023-01-28 01:32:36 +0900

    Add google analytics.

--- front_new/astro.config.mjs
+++ front_new/astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+});
