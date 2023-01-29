b4fe694423c47225a3f931b74b554f30dfffabd5
raahii <piyo56.net@gmail.com>
2023-01-29 15:19:58 +0900

    Refactor top page.

--- front/src/common.ts
+++ front/src/common.ts
@@ -1,4 +1,4 @@
-export const siteName = "haraiai - 払い合い";
+export const siteName = "haraiai (払い合い)";
 export const siteOrigin = "https://haraiai.netlify.app";
 
 export const pageUrlOf = (path: string) => {
